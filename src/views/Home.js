f4b9afd456ab2ec73efc1e4e67d4c432023a3217
qml95 <quentin.morino.laudrin@gmail.com>
2018-01-29 10:35:05 +0100

    add new files home and css

---
+++ src/views/Home.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+
+
+
+
+
+
+class Home extends Component {
+  render() {
+    return (
+        <main className="container">
+          <p>foo</p>
+
+        </main>
+
+    );
+  }
+}
+
+export default Home;
